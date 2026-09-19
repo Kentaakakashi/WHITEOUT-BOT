29d3a18446f69683393dabb863ec3a331776e743
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2026-09-19 08:36:17 +0530

    Implement CWL system initialization function

---
+++ systems/cwlSystem.js
@@ -0,0 +1,9 @@
+const logger = require("../utils/logger");
+
+async function initializeCwlSystem(client) {
+    logger.info("CWL system initialized.");
+}
+
+module.exports = {
+    initializeCwlSystem
+};
