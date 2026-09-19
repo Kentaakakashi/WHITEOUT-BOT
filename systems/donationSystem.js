ce267e799401aab45da7e7974d7e84bd10bc8e46
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2026-09-19 08:37:00 +0530

    Implement donation system initialization function

---
+++ systems/donationSystem.js
@@ -0,0 +1,9 @@
+const logger = require("../utils/logger");
+
+async function initializeDonationSystem(client) {
+    logger.info("Donation system initialized.");
+}
+
+module.exports = {
+    initializeDonationSystem
+};
