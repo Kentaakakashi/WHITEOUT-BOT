2262af3377388a8e3b10f793c64de9e23f076588
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2026-09-19 08:37:41 +0530

    Implement ranking system initialization function

---
+++ systems/rankingSystem.js
@@ -0,0 +1,9 @@
+const logger = require("../utils/logger");
+
+async function initializeRankingSystem(client) {
+    logger.info("Ranking system initialized.");
+}
+
+module.exports = {
+    initializeRankingSystem
+};
