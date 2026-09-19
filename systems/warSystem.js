f57dd2476e25e72ed7161ccf0c3a6beaa287f678
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2026-09-19 08:35:54 +0530

    Implement war system initialization function

---
+++ systems/warSystem.js
@@ -0,0 +1,9 @@
+const logger = require("../utils/logger");
+
+async function initializeWarSystem(client) {
+    logger.info("War system initialized.");
+}
+
+module.exports = {
+    initializeWarSystem
+};
