45b748c59d5522d3ff2f01d0e146b201e32e67ba
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2026-09-19 08:37:13 +0530

    Implement achievement system initialization

---
+++ systems/achievementSystem.js
@@ -0,0 +1,9 @@
+const logger = require("../utils/logger");
+
+async function initializeAchievementSystem(client) {
+    logger.info("Achievement system initialized.");
+}
+
+module.exports = {
+    initializeAchievementSystem
+};
