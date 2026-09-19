ea2a82d02aafff7c18747f029e649911f4dbbcb4
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2026-09-19 08:36:37 +0530

    Implement capital system initialization function

---
+++ systems/capitalSystem.js
@@ -0,0 +1,9 @@
+const logger = require("../utils/logger");
+
+async function initializeCapitalSystem(client) {
+    logger.info("Clan Capital system initialized.");
+}
+
+module.exports = {
+    initializeCapitalSystem
+};
